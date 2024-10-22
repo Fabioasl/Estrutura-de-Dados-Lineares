5d8ca52d8e0be059e779aa9be735f95eef9d396a
Fabioasl <mambafora@HUMNATRN0009.localdomain>
2024-10-22 20:35:32 -0300

    Pilha implementada com array

---
+++ Pilhad_interface.java
@@ -0,0 +1,7 @@
+public interface Pilhad_interface{ // Criando uma interface Pilha
+    public int size();
+    public boolean isEmpty();
+    public Object top();
+    public void push(Object o);
+    public Object pop();
+}
