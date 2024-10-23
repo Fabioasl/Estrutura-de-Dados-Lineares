150a75657ca72e17f47fcb580dad2f1bc5348e80
Fábio Alexandre <163214715+Fabioasl@users.noreply.github.com>
2024-10-22 21:05:13 -0300

    Delete Pilhad_interface.java

--- Pilhad_interface.java
+++
@@ -1,7 +0,0 @@
-public interface Pilhad_interface{ // Criando uma interface Pilha
-    public int size();
-    public boolean isEmpty();
-    public Object top();
-    public void push(Object o);
-    public Object pop();
-}
