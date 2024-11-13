c50546baa8307ff4554c57e27f2b1eaf1ce26293
Fábio Alexandre <163214715+Fabioasl@users.noreply.github.com>
2024-11-13 17:17:29 +0000

    Ajustando o TAD-Vetor

--- TAD-Vetor/Vetor.java
+++ TAD-Vetor/Vetor.java
@@ -1,31 +1,36 @@
+
 public class Vetor implements Vetor_interface{
-    //Atibutos
-    private int tam;
+    private int tamanho;
     private int capacidade;
     private Object [] array;
     public Vetor(int tam){
+        this.tamanho = tam;
         this.capacidade = 0;
-        this.tam = tam;
-        this.array =  new Object[tam];
+        this.array = new Object[tam];
     }
     public boolean isEmpty(){
         return capacidade == 0;
     }
     public int size(){
-        return this.tam;
+        return this.tamanho;
     }
-    public Object elementAtrank(int r) throws Evetorfora{
-        if (r >= size()) throw new Evetorfora('Índice inválido para esse vetor!');
-        return this.array[r];
+    public Object elementAtrank(int index) throws Evetorfora{
+        if (index >= this.array.size()) throw new Evetorfora('O índice está fora do vetor!');
+        return this.array[index];
     }
-    public Object replaceAtrank(int r, Object o){
-        if (r >= array.size()) aumenta_cap();
-        array[r] = o;
-        return array[r];
+    public Object replaceAtrank(int index, Object r){
+        if (index >= capacidade) aumenta_cap();
+    }
+    public void aumenta_cap(){
+        this.capacidade*=2;
+        Object [] novo_array = new Object[capacidade];
+        for (int i = 0; i < array.length(); i++){
+            novo_array[i] = array[i];
+        }
+        array = novo_array;
     }
     public void insertAtrank(int r, Object o){
         if (r >= array.size() || this.tam == this.capacidade) aumenta_cap(); 
-        if (r == array.size() - 1) array[r] = o;
         int antigo = array[r], proximo = array[r + 1];
         array[r] = o;
         for (int i = r + 1; i < array.size(); i++){
@@ -35,19 +40,12 @@ public class Vetor implements Vetor_interface{
         }
         this.capacidade++;
     }
-    public Ojbect removeAtrank(int r) throws Evetorfora{
-            if (r >= array.size()) throw new Evetorfora('Índice inválido para esse vetor!');
-            for (int i = r; i < array.size() - 1; i++){
-                array[r] = array[r+1];
-            }
-            this.capacidade--;
-    }
-    public void aumenta_cap(){
-        this.capacidade *= 2;
-        Object novo_array = new Object[capacidade];
-        for (int i = 0; i < array.size(); i++){
-            novo_array[i] = array[i];
+    public Object removeAtrank(int index) throws new Evetorfora{
+        if (index >= this.array.size()) throw new Evetorfora("O índice está fora do vetor!");
+        for (int i = r; i < array.size() - 1; i++){
+            array[i] = array[i+1]; 
         }
-        array = novo_array;
+        this.capacidade--;
+        return array[index];
     }
-}
+} 
