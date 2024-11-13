941163e3f87e8bfc3db8e639272c3d9859be3893
Fábio Alexandre <163214715+Fabioasl@users.noreply.github.com>
2024-11-13 13:54:27 +0000

    Ajustando o TAD-vetor

--- TAD-Vetor/Vetor.java
+++ TAD-Vetor/Vetor.java
@@ -1,19 +1,53 @@
 public class Vetor implements Vetor_interface{
     //Atibutos
     private int tam;
-    private Object [] array;
     private int capacidade;
+    private Object [] array;
     public Vetor(int tam){
-        this.capacidade = tam;
+        this.capacidade = 0;
         this.tam = tam;
         this.array =  new Object[tam];
     }
     public boolean isEmpty(){
-        return this.array[0] == null;
+        return capacidade == 0;
     }
     public int size(){
-        return this.size;
+        return this.tam;
     }
-    
-
+    public Object elementAtrank(int r) throws Evetorfora{
+        if (r >= size()) throw new Evetorfora('Índice inválido para esse vetor!');
+        return this.array[r];
+    }
+    public Object replaceAtrank(int r, Object o){
+        if (r >= array.size()) aumenta_cap();
+        array[r] = o;
+        return array[r];
+    }
+    public void insertAtrank(int r, Object o){
+        if (r >= array.size() || this.tam == this.capacidade) aumenta_cap(); 
+        if (r == array.size() - 1) array[r] = o;
+        int antigo = array[r], proximo = array[r + 1];
+        array[r] = o;
+        for (int i = r + 1; i < array.size(); i++){
+            proximo = array[i];
+            array[i] = antigo;
+            antigo = proximo;
+        }
+        this.capacidade++;
+    }
+    public Ojbect removeAtrank(int r) throws Evetorfora{
+            if (r >= array.size()) throw new Evetorfora('Índice inválido para esse vetor!');
+            for (int i = r; i < array.size() - 1; i++){
+                array[r] = array[r+1];
+            }
+            this.capacidade--;
+    }
+    public void aumenta_cap(){
+        this.capacidade *= 2;
+        Object novo_array = new Object[capacidade];
+        for (int i = 0; i < array.size(); i++){
+            novo_array[i] = array[i];
+        }
+        array = novo_array;
+    }
 }
--- TAD-Vetor/Vetor_interface.java
+++ TAD-Vetor/Vetor_interface.java
@@ -1,8 +1,8 @@
 public interface Vetor_interface{ // Criando interface vetor
     public boolean isEmpty();
     public int size();
-    public Object elementAtrank(int r);
+    public Object elementAtrank(int r) throws Evetorfora;
     public Object replaceAtrank(int r, Object o );
     public void insertAtrank(int r, Object o);
-    public Object removeAtrank(int r);
+    public Object removeAtrank(int r) throws Evetorfora;
 }
