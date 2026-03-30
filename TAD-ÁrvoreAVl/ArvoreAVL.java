5e120abedcf3cb3346a9165e420f6b67a714f10f
Fabioasl <fabioalexandre07978@gmail.com>
2026-03-30 12:42:11 -0300

    feat: Começa Árvore avl

---
+++ TAD-ÁrvoreAVl/ArvoreAVL.java
@@ -0,0 +1,80 @@
+class No{
+	int valor;
+    int fb;
+	No esquerdo;
+	No direito;
+    No pai;
+	public No(){
+		this.esquerdo = null;
+		this.direito = null;
+        this.pai = null;
+        this.fb = 0;
+	}
+}
+
+public class Arvore{
+	No raiz;
+	public Arvore(){
+		this.raiz = null;
+    }
+    public No raiz(){
+        return raiz;
+    }
+    public  boolean isEmpty(){
+	    return raiz == null;
+    }
+    public void isExternal(No x){
+	    if (x.esquerdo == null && x.direito == null){
+		    return true;
+	    } else{
+		    return false;
+	}
+}
+    public No buscar(int x, No no){
+	    if (no == null) {
+		    return null;
+	    }
+	    if (no.valor == x){
+		    return no;
+	    } 
+	    if (x < no.valor){
+		    return buscar(x, no.esquerdo);
+	    } else{
+		    return buscar(x,no.direito);
+	    }
+    }
+    public No rotacionarEsquerda(No A, no B){
+
+    }
+
+    public No rotacionarDireita(No A, no B){
+
+    }
+
+    // Get e Set
+    public No getEsquerdo(){
+        return No.esquerdo;
+    }
+
+    public No getDireto(){
+        return No.direito;
+    }
+
+    public No getPai(){
+        return No.pai;
+    }
+
+    public void setPai(No noPai){
+        this.noPai = noPai;
+    }
+
+    public void setEsquerdo(No no, int valor){
+        this.no = no;
+        this.valor = valor;
+    }
+
+        public void setDireito(No no, int valor){
+        this.no = no;
+        this.valor = valor;
+    }
+}
